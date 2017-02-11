741ea61fbfaee19a4bea4172d0c932a6b30dbde7
Stephen Wilcox <stephen@tyfone.com>
2017-02-10 21:03:30 -0700

    Specify 5,2 precision for times

--- db/migrate/20141207004005_create_time_entries.rb
+++ db/migrate/20141207004005_create_time_entries.rb
@@ -1,8 +1,8 @@
 class CreateTimeEntries < ActiveRecord::Migration
   def change
     create_table :time_entries do |t|
-      t.decimal :run1
-      t.decimal :run2
+      t.decimal :run1, precision: 5, scale: 2
+      t.decimal :run2, precision: 5, scale: 2
       t.references :week, index: true
       t.references :racer, index: true
       t.timestamps
